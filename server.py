39c2a340fc81c33ab394c88ef2792e7b85af8bd2
IAmrinderSingh <amrindersingh1703@gmail.com>
2022-09-10 22:26:23 +0530

    Formating date of published news

--- server.py
+++ server.py
@@ -8,6 +8,15 @@ app = Flask(__name__)
 baseurl = "https://newsapi.org/v2/"
 
 @app.template_filter()
+def format_datetime(value, format='medium'):
+     # Create a python date object to work on
+    date_obj = datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%SZ')
+    if format == 'full':
+        return date_obj.strftime("%m/%d/%Y, %H:%M:%S")
+    elif format == 'medium':
+        return date_obj.strftime("%d-%b-%Y")
+
+@app.template_filter()
 def capfirst(value):
     return value[:1].upper()+value[1:]
 
